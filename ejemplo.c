208109b3fe3ce34ec1856559455488a4ca2397f9
lissyMp <ymachacca@unsa.edu.pe>
2024-10-03 23:52:08 -0500

    intentamos resolver el problema 2  utilizando la misma tecnica .Llamamos dos ves a la funcion interpreter ,creemos que la segunda llamada se superpone a la primera.

--- ejemplo.c
+++ ejemplo.c
@@ -6,7 +6,9 @@ void display(){
   char** cuadradoN = reverse(cuadradoB);
 
   char** lineaT = repeatH(join(cuadradoB, cuadradoN), 4);
+  char** lineaInversa = repeatH(join(cuadradoN, cuadradoB), 4);
   interpreter(lineaT);
+  interpreter(lineaInversa);
 
 }
 
