f29df2b3a9fbfef6ad6f8a9c2275a54217898305
fernando <fernando@kali.usser>
2024-10-03 23:14:05 -0500

    usando la funcion join juntamos un cuadrado blanco y un cuadrado negro

--- ejemplo.c
+++ ejemplo.c
@@ -2,9 +2,11 @@
 #include "figures.h"
 
 void display(){
-    char** cuadradoB = whiteSquare;
-    char** cuadradoN = reverse(cuadradoB);
+  char** cuadradoB = whiteSquare;
+  char** cuadradoN = reverse(cuadradoB);
 
-    interpreter(reverse(superImpose(queen, cuadradoB)));
+
+  interpreter(join(cuadradoB,cuadradoN));
+
 }
 
