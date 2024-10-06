9e78fddf87579d8c2be2e6a50ab0ad658551eb63
irh1n0c <lhuaynasi@gmail.com>
2024-10-05 22:21:29 -0500

    Probamos la funcion repeatV con la funcion que creamos. Para intentar obtener el tablero completo, no salio como esperabamos.

--- prob03.c
+++ prob03.c
@@ -4,9 +4,9 @@ char** line(){
   return repeatH(join(whiteSquare, reverse(whiteSquare)),4);
 }
 void display (){
-
+  
   char** twoL = up(reverse(line()), line());
-  char** board = repeatV(whiteSquare,2);
+  char** board = repeatV(line(),2);
   interpreter(board);
 
 }
