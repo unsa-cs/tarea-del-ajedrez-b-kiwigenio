3d5dd6bcdc3f51d203eeb835a3d5f6073250df4e
kiwigenio <klaricoh@unsa.edu.pe>
2024-10-05 23:44:39 -0500

    como ya teniamos la primera linea , ahora solo necesitabamos poner el inverso de los figuras , creamos una nueva variable con al patron inverso de ls figuras, salio bien , tuvioms fe :)

---
+++ prob05.c
@@ -0,0 +1,20 @@
+#include "chess.h"
+#include "figures.h"
+char** line(){
+  return repeatH(join(reverse(whiteSquare), whiteSquare),4);
+}
+void display (){
+  
+  char** twoL = up(reverse(line()), line());
+  char** test = join(reverse(whiteSquare),whiteSquare);
+  char** figures1 = join(rook, join(knight, bishop));
+  char** figures2 = join(queen, king);
+  char** figures3 = join(bishop, join(knight, rook));
+
+  char** figures4 = join(figures1, join(figures2, figures3));
+  char** figures4R= reverse(figures4);
+  char** lineaFigura = superImpose(figures4R, line());
+
+  interpreter(lineaFigura);
+
+}
