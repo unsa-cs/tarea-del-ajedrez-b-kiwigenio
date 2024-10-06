b8e9ba0abd9174b611b3641fe0c53e3863dda2ac
lmaoplox <ctaipes@unsa.edu.pe>
2024-10-05 23:34:47 -0500

    Probamos imprimiendo el patron de todas las figuras, usando varias veces la funcion join, salio bien.

--- prob04.c
+++ prob04.c
@@ -7,8 +7,15 @@ void display (){
   
   char** twoL = up(reverse(line()), line());
   char** test = join(reverse(whiteSquare),whiteSquare);
-  char** figures = join(rook, bishop);
+  char** figures1 = join(rook, join(knight, bishop));
+  char** figures2 = join(queen, king);
+  char** figures3 = join(bishop, join(knight, rook));
 
-  interpreter(superImpose(figures, test));
+  char** figures4 = join(figures1, join(figures2, figures3));
+
+
+
+
+  interpreter(figures4);
 
 }
