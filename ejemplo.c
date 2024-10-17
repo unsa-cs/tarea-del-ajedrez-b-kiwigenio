9117092b88fb844729a467827e9f5f3d7752c4a8
irh1n0c <lhuaynasi@gmail.com>
2024-10-16 21:20:05 -0500

    funciona ok

--- ejemplo.c
+++ ejemplo.c
@@ -3,10 +3,10 @@
 
 
 void display(){
+  char** redondo = probado;
   char** triangulo = equilatero;
   char** cuadradoB = whiteSquare;
   char** cuadradoN = reverse(cuadradoB);
-
   char** lineaT = repeatH(join(cuadradoB, cuadradoN), 4);
   char** lineaInversa = repeatH(join(cuadradoN, cuadradoB), 4);
   interpreter(lineaInversa);
